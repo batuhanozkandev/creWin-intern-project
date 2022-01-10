f2b7f8327bdb91034547975653477f15bda910f6
batuhanozkandev <batuhan.ozkan441@gmail.com>
2022-01-10 21:10:13 +0300

    add - firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:crewin_intern_project/view/landing/landingPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
