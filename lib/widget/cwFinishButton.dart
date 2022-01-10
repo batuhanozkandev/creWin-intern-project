2c9e162c4e4735061a7f8d21e57572b19edbfb06
batuhanozkandev <batuhan.ozkan441@gmail.com>
2022-01-11 01:26:25 +0300

    add - firebase methods

---
+++ lib/widget/cwFinishButton.dart
@@ -0,0 +1,26 @@
+import 'package:crewin_intern_project/viewmodel/button/buttonFunction.dart';
+import 'package:flutter/material.dart';
+
+class CwFinishButton extends StatelessWidget {
+  const CwFinishButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      height: height * 0.075,
+      width: width * 0.8,
+      decoration: BoxDecoration(
+          color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+      child: Center(
+        child: Text(
+          "Finish",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
